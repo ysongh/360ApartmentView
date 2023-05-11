7613ccd70c2dd995973c3427a42b5eddf1482339
You Song Hou <ysongweb3@gmail.com>
2023-05-11 01:08:46 -0400

    feat: add a simple server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('It Work'));
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+}); 
