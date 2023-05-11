3d47b43f6beca5d63ea3da0395530e9c1f7856ed
You Song Hou <ysongweb3@gmail.com>
2023-05-10 23:40:29 -0400

    feat: add an example of panorama image with react-pannellum

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
+import ReactPannellum, { getConfig } from "react-pannellum";
 
 function App() {
+  const config = {
+    autoRotate: -2,
+  }
+
+  const click = () => {
+    console.log(getConfig());
+  }
 
   return (
     <>
-      <h1>360ApartmentView</h1>
+      <div>
+        <ReactPannellum
+          id="1"
+          sceneId="firstScene"
+          imageSource="https://pannellum.org/images/alma.jpg"
+          config={config}
+        />
+        <div onClick={click}>Click me</div>
+      </div>
     </>
   )
 }
