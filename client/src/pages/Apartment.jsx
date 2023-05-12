ae98a298e71477383a00fae10a75510dfee32404
You Song Hou <ysongweb3@gmail.com>
2023-05-11 23:48:26 -0400

    feat: add some note icons in the panorama image

--- client/src/pages/Apartment.jsx
+++ client/src/pages/Apartment.jsx
@@ -3,22 +3,37 @@ import ReactPannellum, { getConfig } from "react-pannellum"
 
 function Apartment() {
   const config = {
-    autoRotate: -2,
+    type: 'equirectangular',
+    autoLoad: true,
+    hotSpots: [
+      {
+        type: 'info',
+        pitch: 10,
+        yaw: 20,
+        text: 'A small note'
+      },
+      {
+        type: 'info',
+        pitch: -10,
+        yaw: 30,
+        text: 'This is another note!'
+      }
+    ]
   }
 
   const click = () => {
     console.log(getConfig());
   }
-  
+
   return (
     <div>
       <ReactPannellum
-          id="1"
-          sceneId="firstScene"
-          imageSource="https://pannellum.org/images/alma.jpg"
-          config={config}
-        />
-        <div onClick={click}>Click me</div>
+        id="1"
+        sceneId="firstScene"
+        imageSource="https://pannellum.org/images/alma.jpg"
+        config={config}
+      />
+      <div onClick={click}>Click me</div>
     </div>
   )
 }
