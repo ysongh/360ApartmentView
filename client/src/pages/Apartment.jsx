2b993b64969516c234981cf4f3c3ff47834ed4f9
You Song Hou <ysongweb3@gmail.com>
2023-05-12 01:13:31 -0400

    feat: update the config to open a new tab when clicking on the hotSpots

--- client/src/pages/Apartment.jsx
+++ client/src/pages/Apartment.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactPannellum, { getConfig } from "react-pannellum"
 
+const imageURL1 = "https://www.shutterstock.com/shutterstock/photos/1175752585/display_1500/stock-photo--d-illustration-spherical-vr-degrees-a-seamless-panorama-of-the-room-and-room-of-light-1175752585.jpg"
+const imageURL2 = "https://www.shutterstock.com/shutterstock/photos/525626881/display_1500/stock-photo-hdri-panorama-view-in-modern-white-empty-loft-apartment-interior-of-living-room-hall-full-525626881.jpg"
+
 function Apartment() {
+  const [image, setImage] = useState(imageURL1);
+
   const config = {
     type: 'equirectangular',
     autoLoad: true,
     hotSpots: [
       {
+        id: "1",
         type: 'info',
         pitch: 10,
         yaw: 20,
-        text: 'A small note'
+        text: 'A small note',
+        URL: imageURL1,
       },
       {
+        id: "2",
         type: 'info',
         pitch: -10,
         yaw: 30,
-        text: 'This is another note!'
+        text: 'This is another note!',
+        URL: imageURL2
       }
     ]
   }
 
   const click = () => {
-    console.log(getConfig());
+    console.log(getConfig())
   }
 
   return (
@@ -30,7 +39,7 @@ function Apartment() {
       <ReactPannellum
         id="1"
         sceneId="firstScene"
-        imageSource="https://pannellum.org/images/alma.jpg"
+        imageSource={image}
         config={config}
       />
       <div onClick={click}>Click me</div>
