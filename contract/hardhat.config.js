b00ef12a4bacbfcb886968650476708d6cb4079d
You Song Hou <ysongweb3@gmail.com>
2023-05-17 01:08:33 -0400

    fix: install '@tableland/local' to compile and deploy contracts

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -5,10 +5,13 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  // npx hardhat run scripts/deploy.js --network localhost
   localTableland: {
     silent: false,
     verbose: false,
   },
+
+  // npx hardhat run scripts/deploy.js --network filecoin-hyperspace
   networks: {
     "filecoin-hyperspace": {
       url: "https://rpc.ankr.com/filecoin_testnet",
