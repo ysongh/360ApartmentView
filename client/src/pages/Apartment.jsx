010691b62bf1f689b868e3c0601e772a41547af1
You Song Hou <ysongweb3@gmail.com>
2023-05-19 00:53:39 -0400

    feat: add Chakra UI Tabs

--- client/src/pages/Apartment.jsx
+++ client/src/pages/Apartment.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { FormControl, FormLabel, Input, Button } from '@chakra-ui/react'
+import { Tabs, Tab, TabPanels, TabPanel, TabList, FormControl, FormLabel, Input, Button } from '@chakra-ui/react'
 import ReactPannellum, { getConfig, addHotSpot } from "react-pannellum"
 
 const imageURL = [
@@ -78,18 +78,41 @@ function Apartment() {
         imageSource={imageURL[id]}
         config={config}
       />
-      <Button onClick={debug} mt={3}>Check Config</Button>
-      <br />
-      <FormControl mb='3'>
-        <FormLabel htmlFor='message'>Add Note</FormLabel>
-        <Input value={message} onChange={(e) => setMessage(e.target.value)} />
-      </FormControl>
-      <FormControl mb='3'>
-        <FormLabel htmlFor='url'>Add Scene</FormLabel>
-        <Input value={url} onChange={(e) => setURL(e.target.value)} />
-      </FormControl>
-      <Button onClick={addNote} mt={3}>Add Note</Button>
-      <Button onClick={addScene} mt={3}>Add Scene</Button>
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>Add Note</Tab>
+          <Tab>Add Scene</Tab>
+          <Tab>Debug</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <FormControl mb='3'>
+              <FormLabel htmlFor='message'>Add Note</FormLabel>
+              <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+            </FormControl>
+
+            <Button onClick={addNote} mt={3}>Add Note</Button>
+          </TabPanel>
+
+          <TabPanel>
+            <FormControl mb='3'>
+              <FormLabel htmlFor='message'>Add Note</FormLabel>
+              <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+            </FormControl>
+            <FormControl mb='3'>
+              <FormLabel htmlFor='url'>Add Scene</FormLabel>
+              <Input value={url} onChange={(e) => setURL(e.target.value)} />
+            </FormControl>
+            
+            <Button onClick={addScene} mt={3}>Add Scene</Button>
+          </TabPanel>
+
+          <TabPanel>
+            <Button onClick={debug} mt={3}>Check Config</Button>
+          </TabPanel>
+          
+        </TabPanels>
+      </Tabs>
     </div>
   )
 }
