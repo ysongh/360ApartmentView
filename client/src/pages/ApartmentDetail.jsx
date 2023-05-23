e4ff3efb2052b32fb30b4f0d8f882fd81bf4ebfa
You Song Hou <ysongweb3@gmail.com>
2023-05-22 23:42:51 -0400

    feat: remove the editor from the ApartmentDetail page

--- client/src/pages/ApartmentDetail.jsx
+++ client/src/pages/ApartmentDetail.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect ,useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Tabs, Tab, TabPanels, TabPanel, TabList, FormControl, FormLabel, Input, Button } from '@chakra-ui/react'
-import { getConfig, addHotSpot } from "react-pannellum"
+import {  addHotSpot } from "react-pannellum"
 
 import { getApartmentByID } from "../Tableland"
 import Image360 from '../components/Image360'
@@ -9,8 +8,6 @@ import Image360 from '../components/Image360'
 function ApartmentDetail() {
   const { id } = useParams()
 
-  const [message, setMessage] = useState("")
-  const [url, setURL] = useState("")
   const [imageURL, setImageURL] = useState(null)
 
   useEffect(() => {
@@ -39,76 +36,9 @@ function ApartmentDetail() {
     }
   }
 
-  const debug = () => {
-    console.log(getConfig())
-  }
-
-  const addNote = () => {
-    const config = getConfig()
-
-    addHotSpot({
-      "pitch": config.pitch,
-      "yaw": config.yaw,
-      "type": "info",
-      "text": message
-    }, "firstScene")
-
-    setMessage("")
-  }
-
-  const addScene = () => {
-    const config = getConfig()
-
-    addHotSpot({
-      "pitch": config.pitch,
-      "yaw": config.yaw,
-      "type": "scene",
-      "text": message,
-      "URL": url
-    }, "firstScene")
-
-    setMessage("")
-    setURL("")
-  }
-
   return (
     <div>
       {imageURL && <Image360 currentImage={imageURL} /> }
-      <Tabs isFitted variant='enclosed'>
-        <TabList mb='1em'>
-          <Tab>Add Note</Tab>
-          <Tab>Add Scene</Tab>
-          <Tab>Debug</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <FormControl mb='3'>
-              <FormLabel htmlFor='message'>Add Note</FormLabel>
-              <Input value={message} onChange={(e) => setMessage(e.target.value)} />
-            </FormControl>
-
-            <Button onClick={addNote} mt={3}>Add Note</Button>
-          </TabPanel>
-
-          <TabPanel>
-            <FormControl mb='3'>
-              <FormLabel htmlFor='message'>Add Note</FormLabel>
-              <Input value={message} onChange={(e) => setMessage(e.target.value)} />
-            </FormControl>
-            <FormControl mb='3'>
-              <FormLabel htmlFor='url'>Add Scene</FormLabel>
-              <Input value={url} onChange={(e) => setURL(e.target.value)} />
-            </FormControl>
-            
-            <Button onClick={addScene} mt={3}>Add Scene</Button>
-          </TabPanel>
-
-          <TabPanel>
-            <Button onClick={debug} mt={3}>Check Config</Button>
-          </TabPanel>
-          
-        </TabPanels>
-      </Tabs>
     </div>
   )
 }
