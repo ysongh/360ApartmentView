784aae9f02dd21c5cfdf55c7b2c23fe116a7a707
You Song Hou <ysongweb3@gmail.com>
2023-05-23 22:30:08 -0400

    feat: display the apartment detail below the panorama image

--- client/src/pages/ApartmentDetail.jsx
+++ client/src/pages/ApartmentDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect ,useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {  addHotSpot } from "react-pannellum"
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import { addHotSpot } from "react-pannellum"
 
 import { getApartmentByID } from "../Tableland"
 import Image360 from '../components/Image360'
@@ -9,6 +10,7 @@ function ApartmentDetail() {
   const { id } = useParams()
 
   const [imageURL, setImageURL] = useState(null)
+  const [apartment, setApartment] = useState({})
 
   useEffect(() => {
     getApartment()
@@ -19,6 +21,7 @@ function ApartmentDetail() {
       const newApartment = await getApartmentByID(id)
       console.log(newApartment)
       setImageURL(newApartment[0].apt_url)
+      setApartment(newApartment[0])
 
       const response = await fetch(newApartment[0].data_url)
       let data = await response.json()
@@ -39,6 +42,13 @@ function ApartmentDetail() {
   return (
     <div>
       {imageURL && <Image360 currentImage={imageURL} /> }
+      
+      <Flex justifyContent="space-between" alignItems="center">
+        <Heading fontSize="3xl">{apartment.location}</Heading>
+        <Text textAlign="left" fontSize="xl">${apartment.price} / month</Text>
+      </Flex>
+      <Text textAlign="left" fontSize="xl">{apartment.number_of_rooms} rooms</Text>
+      
     </div>
   )
 }
