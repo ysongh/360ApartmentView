ea7a8a71ed72d83389edcb4a37da7e67cfaa23ac
You Song Hou <ysongweb3@gmail.com>
2023-05-24 22:09:19 -0400

    feat: display the name of the chain

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import { Link as ReactLink } from 'react-router-dom'
-import { Container, Box, Flex, Heading, Spacer, Button, Link } from '@chakra-ui/react'
+import { Container, Box, Flex, Heading, Spacer, Badge, Button, Link } from '@chakra-ui/react'
 import { ethers } from 'ethers'
 
 import ApartmentFinder from '../../artifacts/contracts/Apartment360.sol/Apartment360.json'
@@ -7,6 +8,8 @@ import ApartmentFinder from '../../artifacts/contracts/Apartment360.sol/Apartmen
 const CONTRACT_ADDRESS = "0xEA4d06C809B789aD4Ce397B5995b3f7039a9b228";
 
 function Navbar({ ethAddress, setETHAddress, setContract360AF }) {
+  const [chainName, setChainName] = useState('');
+
   const connectMetamask = async () => {
     try{
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -18,6 +21,7 @@ function Navbar({ ethAddress, setETHAddress, setContract360AF }) {
       const contract = new ethers.Contract(CONTRACT_ADDRESS, ApartmentFinder.abi, signer);
       console.log(contract);
       setContract360AF(contract);
+      setChainName("Hyperspace");
 
     } catch(error) {
       console.error(error);
@@ -37,6 +41,7 @@ function Navbar({ ethAddress, setETHAddress, setContract360AF }) {
           <Link as={ReactLink} to="/apartment/0">Apartment</Link>
           <Link as={ReactLink} to="/addapartment">Add Apartment</Link>
           <Spacer />
+          {chainName && <p><Badge bgColor="green.300" fontSize='.9rem'>{chainName}</Badge></p>}
           <Button onClick={connectMetamask}>
             {ethAddress ? ethAddress.slice(0, 5) + "..." + ethAddress.slice(37, 42) : 'Connect Wallet'}
           </Button>
