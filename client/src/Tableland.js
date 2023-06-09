7353420d6de69e627ddc7e518939a3900e57df16
You Song Hou <ysongweb3@gmail.com>
2023-06-08 23:02:56 -0400

    feat: filter apartments by non-expired date

--- client/src/Tableland.js
+++ client/src/Tableland.js
@@ -1,11 +1,15 @@
 import { Database } from "@tableland/sdk"
 
+import { dateToUnixTime } from "./utils/format"
+
 const db = new Database()
 const tableName = "my_hardhat_table_80001_6764"
 
 export const getApartments = async () => {
+
   try {
-    const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all()
+    const currentUnixTime = await dateToUnixTime()
+    const { results } = await db.prepare(`SELECT * FROM ${tableName} WHERE expire_date > '${currentUnixTime}';`).all()
     console.log(results)
     return results
   } catch (error) {
--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -3,3 +3,10 @@ export const formatDate = async (timestamp) => {
   const formattedDate = date.toLocaleString()
   return formattedDate
 }
+
+export const dateToUnixTime = async () => {
+  const date = new Date()
+  const unixTime = Math.floor(date.getTime() / 1000)
+  console.log(unixTime)
+  return unixTime
+}
